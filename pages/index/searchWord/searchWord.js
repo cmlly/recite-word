cfd6fdf64020032542b527bffde10a56c7cc6180
unknown <jsliu@cobocn.net>
2018-12-21 17:18:13 +0800

    更改了首页

---
+++ pages/index/searchWord/searchWord.js
@@ -0,0 +1,32 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+        inputShowed: false,
+        inputVal: ""
+    },
+    showInput: function () {
+        this.setData({
+            inputShowed: true
+        });
+    },
+    hideInput: function () {
+        this.setData({
+            inputVal: "",
+            inputShowed: false
+        });
+    },
+    clearInput: function () {
+        this.setData({
+            inputVal: ""
+        });
+    },
+    inputTyping: function (e) {
+        this.setData({
+            inputVal: e.detail.value
+        });
+    }
+  
+})
